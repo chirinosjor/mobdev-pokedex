2585d3122538cdbb51a5043096645a60908792b0
Jorge Chirinos <chirinosjor@gmail.com>
2024-10-29 12:31:18 -0300

    fix: peer dependencies

--- src/components/PokemonList/components/PokemonModalContent/components/TypesBadges/index.tsx
+++ src/components/PokemonList/components/PokemonModalContent/components/TypesBadges/index.tsx
@@ -3,24 +3,27 @@ import { PokemonType } from '@services/pokemonDetail';
 import { capitalizeFirstLetter } from '@utils/string';
 
 function TypesBadges({ types }: { types: PokemonType[]; }) {
+
+  if (!types || types.length === 0) {
+    return null;
+  }
+
   return (
-    types && types.length > 0 && (
-      <div className="flex gap-2 my-2">
-        {types.map(({ type }) => {
-          const typeKey = type.name as PokemonTypeKey;
-          const typeColor = POKEMON_TYPE_COLORS[typeKey];
-          return (
-            <span
-              style={{ backgroundColor: typeColor?.normal }}
-              className="text-white px-4 py-1 rounded-full text-sm"
-              key={type.name}
-            >
-              {capitalizeFirstLetter(type.name)}
-            </span>
-          );
-        })}
-      </div>
-    )
+    <div className="flex gap-2 my-2">
+      {types.map(({ type }) => {
+        const typeKey = type.name as PokemonTypeKey;
+        const typeColor = POKEMON_TYPE_COLORS[typeKey];
+        return (
+          <span
+            style={{ backgroundColor: typeColor?.normal }}
+            className="text-white px-4 py-1 rounded-full text-sm"
+            key={type.name}
+          >
+            {capitalizeFirstLetter(type.name)}
+          </span>
+        );
+      })}
+    </div>
   );
 }
 
